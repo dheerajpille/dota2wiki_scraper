ab6f4e57ce99117dae9550ae53e61f9e7ad21173
dheerajpille <dheerajpille@gmail.com>
2017-10-23 17:19:48 -0400

    Added talent XPath

--- dota2wiki_scraper/spiders/dota2wiki.py
+++ dota2wiki_scraper/spiders/dota2wiki.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from w3lib.html import remove_tags
 
 class Dota2wikiSpider(scrapy.Spider):
     name = 'dota2wiki'
@@ -10,7 +11,8 @@ class Dota2wikiSpider(scrapy.Spider):
         self.start_urls = ['https://dota2.gamepedia.com/%s' % kwargs.get('hero')]
 
     def parse(self, response):
-        lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td').extract()
+        lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td/text()').extract()
+        talent = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "wikitable", " " ))]//td').extract()
 
         # TODO: yield/return this later
-        print(lore)
+        print(talent)
