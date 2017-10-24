65b97f092a7a6e8f643df6986c5bf769f0fa4b11
dheerajpille <dheerajpille@gmail.com>
2017-10-23 22:17:00 -0400

    Implemented PrettyTable for talent tree

--- dota2wiki_scraper/spiders/dota2wiki.py
+++ dota2wiki_scraper/spiders/dota2wiki.py
@@ -16,12 +16,23 @@ class Dota2wikiSpider(scrapy.Spider):
         lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//'
                               'tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td/text()').extract()
 
+        table = PrettyTable(['Talent 1', 'Level', 'Talent 2'])
+
         # Talent tree XPath
+        # TODO: get text from spans and hyperlinks
         talent = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "wikitable", " " ))]'
                                 '//td/text()').extract()
 
         while "\n" in talent:
             talent.remove("\n")
 
+        level = 25
+
+        while level >= 10:
+            table.add_row([talent[0], level, talent[1]])
+            level -= 5
+
         # TODO: yield/return these
+        print(lore)
         print(talent)
+        print(table)
