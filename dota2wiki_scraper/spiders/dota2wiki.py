84e75a93dd614b75789d89b93e75aaab139cc4f5
dheerajpille <dheerajpille@gmail.com>
2017-10-30 15:28:10 -0400

    Added cast animation values together

--- dota2wiki_scraper/spiders/dota2wiki.py
+++ dota2wiki_scraper/spiders/dota2wiki.py
@@ -136,17 +136,21 @@ class Dota2wikiSpider(scrapy.Spider):
             if "Modifiers" in ability_keys_data[i]:
                 ability_keys_data[i] = ability_keys_data[i][:ability_keys_data[i].index("Modifiers")-1]
 
-        value_index = 0
-
         # TODO: use del_row(int) to delete row from PrettyTable
         ability_table = PrettyTable(['Ability Name', ability[index]])
 
         while True:
+            value_index = 0
             for i in range(int(len(ability_header_data[index])/2)):
                 ability_table.add_row([ability_header_data[index][i*2], ability_header_data[index][i*2+1]])
             for i in range(len(ability_keys_data[index])):
-                ability_table.add_row([ability_keys_data[index][i], ability_values[value_index]])
-                value_index += 1
+                if ability_keys_data[index][i] == "Cast Animation":
+                    ability_table.add_row([ability_keys_data[index][i], ability_values[value_index]+'+'+
+                                           ability_values[value_index+1]])
+                    value_index += 2
+                else:
+                    ability_table.add_row([ability_keys_data[index][i], ability_values[value_index]])
+                    value_index += 1
             break
 
         print(ability_table)
