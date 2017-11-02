e67283603624957ef014e88bebf579e3d84c60c4
dheerajpille <dheerajpille@gmail.com>
2017-11-02 11:37:43 -0400

    Converted various parsing methods to static

--- dota2wiki_scraper/spiders/dota2wiki.py
+++ dota2wiki_scraper/spiders/dota2wiki.py
@@ -13,6 +13,98 @@ class Dota2wikiSpider(scrapy.Spider):
         self.start_urls = ['https://dota2.gamepedia.com/%s' % kwargs.get('hero')]
 
     def parse(self, response):
+        # Defines hero as Hero item defined in items.py
+        hero = Hero()
+
+        hero['title'] = self.parse_title(response)
+        hero['lore'] = self.parse_lore(response)
+        hero['stat_gain'] = self.parse_stat_gain(response)
+        hero['data'] = self.parse_data(response)
+        hero['misc_data'] = self.parse_misc_data(response)
+        hero['abilities'] = self.parse_abilities(response)
+        hero['talent_tree'] = self.parse_talent_tree(response)
+
+        print(hero)
+
+    @staticmethod
+    def parse_title(response):
+
+        # Hero title without leading and trailing spaces
+        title = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//tr'
+                               '[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//th'
+                               '/text()').extract()[1].strip()
+
+        return title
+
+    @staticmethod
+    def parse_lore(response):
+
+        # Hero lore XPath
+        lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//'
+                              'tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td/text()').extract()
+
+        return lore
+
+    @staticmethod
+    def parse_stat_gain(response):
+
+        stat_table = PrettyTable(['STR Gain', 'AGI Gain', 'INT Gain'])
+
+        stat = response.xpath('//tr[(((count(preceding-sibling::*) + 1) = 3) and parent::*)]//tr//td'
+                              '//text()').extract()
+
+        while ' ' in stat:
+            stat.remove(' ')
+
+        stat_table.add_row([stat[0] + stat[1],
+                            stat[2] + stat[3],
+                            stat[4] + stat[5]])
+
+        return stat_table
+
+    @staticmethod
+    def parse_data(response):
+        header = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", " " ))]'
+                                '//th[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//text()').extract()
+
+        header = [x.strip() for x in header]
+
+        while '' in header:
+            header.remove('')
+
+        data_table = PrettyTable([header[0], 'Base', '1', '15', '25'])
+
+        data = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", " " ))]//td'
+                              '//text()').extract()
+
+        index = 0
+
+        level_base = []
+        level_1 = []
+        level_15 = []
+        level_25 = []
+
+        while index < len(data):
+            if index % 4 == 0:
+                level_base.append(data[index])
+            elif index % 4 == 1:
+                level_1.append(data[index])
+            elif index % 4 == 2:
+                level_15.append(data[index])
+            else:
+                level_25.append(data[index])
+            index += 1
+
+        index = 0
+
+        while index < len(header) - 1:
+            data_table.add_row([header[index + 1], level_base[index], level_1[index], level_15[index], level_25[index]])
+            index += 1
+
+        yield(data_table)
+
+    @staticmethod
+    def parse_misc_data(response):
 
         misc_key = response.xpath('//tr[(((count(preceding-sibling::*) + 1) = 5) and parent::*)]//*'
                                   '[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", '
@@ -54,133 +146,20 @@ class Dota2wikiSpider(scrapy.Spider):
                 misc_val.append(misc_val_raw[index])
                 index += 1
 
-        misc_table = PrettyTable(['Key', 'Value'])
+        misc_data_table = PrettyTable(['Key', 'Value'])
         index = 0
 
         while index < len(misc_key):
-            misc_table.add_row([misc_key[index], misc_val[index]])
+            misc_data_table.add_row([misc_key[index], misc_val[index]])
             index += 1
 
-        misc_table.align = "l"
-        print(misc_table)
+        # Aligns table to left
+        misc_data_table.align = "l"
 
+        return misc_data_table
 
-    def parse_title(self, response):
-
-        # Hero title without leading and trailing spaces
-        title = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//tr'
-                               '[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//th'
-                               '/text()').extract()[1].strip()
-
-        return title
-
-    def parse_lore(self, response):
-
-        # Hero lore XPath
-        lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//'
-                              'tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td/text()').extract()
-
-        return lore
-
-    def parse_stat_gain(self, response):
-
-        stat_table = PrettyTable(['STR Gain', 'AGI Gain', 'INT Gain'])
-
-        stat = response.xpath('//tr[(((count(preceding-sibling::*) + 1) = 3) and parent::*)]//tr//td'
-                              '//text()').extract()
-
-        while ' ' in stat:
-            stat.remove(' ')
-
-        stat_table.add_row([stat[0] + stat[1],
-                            stat[2] + stat[3],
-                            stat[4] + stat[5]])
-
-        return stat_table
-
-    def parse_data(self, response):
-        header = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", " " ))]'
-                                '//th[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//text()').extract()
-
-        header = [x.strip() for x in header]
-
-        while '' in header:
-            header.remove('')
-
-        data_table = PrettyTable([header[0], 'Base', '1', '15', '25'])
-
-        data = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", " " ))]//td'
-                              '//text()').extract()
-
-        index = 0
-
-        level_base = []
-        level_1 = []
-        level_15 = []
-        level_25 = []
-
-        while index < len(data):
-            if index % 4 == 0:
-                level_base.append(data[index])
-            elif index % 4 == 1:
-                level_1.append(data[index])
-            elif index % 4 == 2:
-                level_15.append(data[index])
-            else:
-                level_25.append(data[index])
-            index += 1
-
-        index = 0
-
-        while index < len(header) - 1:
-            data_table.add_row([header[index + 1], level_base[index], level_1[index], level_15[index], level_25[index]])
-            index += 1
-
-        yield(data_table)
-
-    def parse_misc_data(self, response):
-
-        misc_table = PrettyTable(['Key', 'Value'])
-
-        misc_key = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "oddrowsgray", " " ))]//th'
-                                  '//text()').extract()
-
-        while ' ' in misc_key:
-            misc_key.remove(' ')
-
-        while '\n' in misc_key:
-            misc_key.remove('\n')
-
-        misc_val_raw = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "oddrowsgray", " " ))]//td'
-                                      '//text()').extract()
-
-        while ' ' in misc_val_raw:
-            misc_val_raw.remove(' ')
-
-        while '\n' in misc_val_raw:
-            misc_val_raw.remove('\n')
-
-        misc_val = []
-        index = 0
-
-        while index < len(misc_val_raw):
-            if misc_val_raw[index] == '/' or misc_val_raw[index] == '+':
-                misc_val.pop()
-                misc_val.append(''.join(misc_val_raw[index - 1:index + 2]))
-                index += 2
-            else:
-                misc_val.append(misc_val_raw[index])
-                index += 1
-
-        index = 0
-
-        while index < 10:
-            misc_table.add_row([misc_key[index], misc_val[index]])
-            index += 1
-
-        return misc_table
-
-    def parse_abilities(self, response):
+    @staticmethod
+    def parse_abilities(response):
         # Normal abilities
         ability_normal = response.xpath('//*[(@id = "mw-content-text")]//div//div//div[contains(@style, "font-weight: '
                                         'bold; font-size: 110%; border-bottom: 1px solid black; background-color: '
@@ -420,7 +399,8 @@ class Dota2wikiSpider(scrapy.Spider):
 
         return ability_dict
 
-    def parse_talent_tree(self, response):
+    @staticmethod
+    def parse_talent_tree(response):
 
         talent_table = PrettyTable(['Talent Left', 'Level', 'Talent Right'])
 
