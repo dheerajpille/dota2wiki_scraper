3f59116407d5fbb7700154c262e2bed4d36a6f12
dheerajpille <dheerajpille@gmail.com>
2017-11-02 10:22:54 -0400

    Began configuring Hero item to parsed values

--- dota2wiki_scraper/spiders/dota2wiki.py
+++ dota2wiki_scraper/spiders/dota2wiki.py
@@ -3,7 +3,7 @@ import scrapy
 
 from prettytable import PrettyTable
 
-from dota2wiki_scraper.items import Dota2WikiScraperItem
+from dota2wiki_scraper.items import Hero
 
 class Dota2wikiSpider(scrapy.Spider):
     name = 'dota2wiki'
@@ -14,243 +14,54 @@ class Dota2wikiSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        # Normal abilities
-        ability_normal = response.xpath('//*[(@id = "mw-content-text")]//div//div//div[contains(@style, "font-weight: '
-                                        'bold; font-size: 110%; border-bottom: 1px solid black; background-color: '
-                                        '#B44335; color: white; padding: 3px 5px;")]/text()').extract()
+        misc_table = PrettyTable(['Key', 'Value'])
 
-        # Removes unnecessary values from normal abilities
-        while '\n' in ability_normal:
-            ability_normal.remove('\n')
+        misc_key = response.xpath('//tr[(((count(preceding-sibling::*) + 1) = 5) and parent::*)]//*'
+                                  '[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", '
+                                  '" " ))]//th//text()').extract()
 
-        # Ultimate ability
-        ability_ult = response.xpath('//*[(@id = "mw-content-text")]//div//div//div[contains(@style, "font-weight: '
-                                     'bold; font-size: 110%; border-bottom: 1px solid black; background-color: '
-                                     '#414141; color: white; padding: 3px 5px;")]/text()').extract()
+        while ' ' in misc_key:
+            misc_key.remove(' ')
 
-        # Removes unnecessary values from ultimate ability
-        while '\n' in ability_ult:
-            ability_ult.remove('\n')
+        while '\n' in misc_key:
+            misc_key.remove('\n')
 
-        # Combines normal and ultimate abilities
-        ability = ability_normal + ability_ult
+        for i in range(len(misc_key)):
+            misc_key[i] = misc_key[i].strip('\n')
 
-        # Retrieves all bold keys from abilities
-        ability_keys_raw = response.xpath('//*[(@id = "mw-content-text")]//div//div//div//b//text()').extract()
+        misc_val_raw = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "oddrowsgray", " " ))]//td'
+                                      '//text()').extract()
 
-        # Retrieves all span values from abilities
-        ability_values_raw = response.xpath('//*[(@id = "mw-content-text")]//div//div//div//span//text()').extract()
+        print(misc_key)
 
-        # Removes whitespace values from ability_values
-        while ' ' in ability_values_raw:
-            ability_values_raw.remove(' ')
+        while ' ' in misc_val_raw:
+            misc_val_raw.remove(' ')
+
+        while '\n' in misc_val_raw:
+            misc_val_raw.remove('\n')
+
+        print(misc_val_raw)
+
+        misc_val = []
+        index = 0
+
+        while index < len(misc_val_raw):
+            if misc_val_raw[index] == '/' or misc_val_raw[index] == '+':
+                misc_val.pop()
+                misc_val.append(''.join(misc_val_raw[index - 1:index + 2]))
+                index += 2
+            else:
+                misc_val.append(misc_val_raw[index])
+                index += 1
 
         index = 0
-        ability_values = []
 
-        # Removes brackets (AGHANIM'S/TALENT MODIFIERS) and keys from ability values
-        while index < len(ability_values_raw):
-            # Removes values in brackets
-            if ability_values_raw[index][-1] == '(':
-                ability_values.append(ability_values_raw[index][:-1])
-                index += 1
-                while True:
-                    if ability_values_raw[index][-1] == ')':
-                        break
-                    index += 1
-            # Checks for numerical values or Global range (exception case)
-            elif ability_values_raw[index][0].isdigit() or "Global" in ability_values_raw[index]:
-                ability_values.append(ability_values_raw[index].strip())
+        while index < 10:
+            misc_table.add_row([misc_key[index], misc_val[index]])
             index += 1
 
-        # Removes all None values in list
-        ability_values = list(filter(None, ability_values))
+        print(misc_table)
 
-        cd_mana_raw = response.xpath('//*[(@id = "mw-content-text")]//div//div//div//text()').extract()
-
-        for i in range(len(cd_mana_raw)):
-            cd_mana_raw[i] = cd_mana_raw[i].strip().replace('\xa0', '')
-
-        while '' in cd_mana_raw:
-            cd_mana_raw.remove('')
-
-        while 'Play' in cd_mana_raw:
-            cd_mana_raw.remove('Play')
-
-        index = 0
-        cd_mana_clean = []
-
-        while index < len(cd_mana_raw):
-            if cd_mana_raw[index][-1] == '(':
-                if cd_mana_raw[index] != '(':
-                    cd_mana_clean.append(cd_mana_raw[index].strip('('))
-                while True:
-                    index += 1
-                    if cd_mana_raw[index][-1] == ')':
-                        index += 1
-                        break
-            else:
-                cd_mana_clean.append(cd_mana_raw[index])
-                index += 1
-
-        cd_mana_indices = [item for item in range(len(cd_mana_clean)) if cd_mana_clean[item] == "Ability"]
-
-        cd_mana_data = []
-
-        # CD/Mana cost values
-        while cd_mana_indices:
-
-            cd_mana_list = []
-            index = cd_mana_indices[0]
-
-            while index < len(cd_mana_clean):
-                if len(cd_mana_indices) > 1:
-                    if index == cd_mana_indices[1]:
-                        break
-                else:
-                    if index == len(cd_mana_indices):
-                        break
-
-                if cd_mana_clean[index] == ':' or cd_mana_clean[index] == '+':
-                    index += 2
-                    continue
-                elif cd_mana_clean[index][0].isdigit():
-                    cd_mana_list.append(cd_mana_clean[index])
-                index += 1
-
-            start = 0
-            end = 0
-            cd_mana_list_clean = []
-
-            while end < len(cd_mana_list):
-                if cd_mana_list[end][-1].isdigit():
-                    cd_mana_list_clean.append(''.join(cd_mana_list[start:end+1]))
-                    start = end+1
-                end += 1
-
-            cd_mana_data.append(cd_mana_list_clean)
-            cd_mana_indices.pop(0)
-
-        # Ability, Affects, and Damage values found in ability header
-        ability_header_raw = response.xpath('//*[(@id = "mw-content-text")]//div//div//div[contains(@style, "display: '
-                                            'inline-block; width: 32%; vertical-align: top;")]//text()').extract()
-
-        while ' ' in ability_header_raw:
-            ability_header_raw.remove(' ')
-
-        index = 0
-        ability_header_clean = []
-
-        # Cleans ability header list
-        while index < len(ability_header_raw):
-            # Appends multiple type values separated by '/' together
-            if ability_header_raw[index].strip() == '/':
-                ability_header_clean.pop()
-                ability_header_clean.append(ability_header_raw[index-1].strip() + ' ' +
-                                            ability_header_raw[index].strip() + ' ' +
-                                            ability_header_raw[index+1].strip())
-                index += 2
-            elif ability_header_raw[index].strip()[-1] == '/':
-                ability_header_clean.append(ability_header_raw[index] + ability_header_raw[index+1])
-                index += 2
-            elif ability_header_raw[index].strip()[0] == '/':
-                ability_header_clean.pop()
-                ability_header_clean.append(ability_header_raw[index-1] + ability_header_raw[index])
-                index += 1
-            # Removes values inside brackets from list
-            elif ability_header_raw[index] == '(':
-                index += 1
-                while True:
-                    if ability_header_raw[index] == ')':
-                        index += 1
-                        break
-                    index += 1
-            if index < len(ability_header_raw):
-                ability_header_clean.append(ability_header_raw[index].strip())
-            index += 1
-
-        ability_header_data = []
-
-        # Ability header indices, used to slice list to sub-lists
-        ability_indices = [item for item in range(len(ability_header_clean)) if ability_header_clean[item] == "Ability"]
-
-        # Slices ability header list using indices provided above
-        for i in range(len(ability_indices)):
-            if i != len(ability_indices)-1:
-                ability_header_data.append(ability_header_clean[ability_indices[i]:ability_indices[i+1]])
-            else:
-                ability_header_data.append(ability_header_clean[ability_indices[i]:])
-
-        # Ability key indices, used to slice list to sub-lists
-        ability_indices = [item for item in range(len(ability_keys_raw)) if ability_keys_raw[item] == "Ability"]
-
-        ability_keys_data = []
-
-        # Slices ability key list using indices provided above
-        for i in range(len(ability_indices)):
-            if i != len(ability_indices)-1:
-                ability_keys_data.append(ability_keys_raw[ability_indices[i]:ability_indices[i+1]])
-            else:
-                ability_keys_data.append(ability_keys_raw[ability_indices[i]:])
-
-        # Removes ability headers and modifiers from sub-lists if applicable
-        for i in range(len(ability_keys_data)):
-            if "Ability" in ability_keys_data[i]:
-                ability_keys_data[i].remove("Ability")
-            if "Affects" in ability_keys_data[i]:
-                ability_keys_data[i].remove("Affects")
-            if "Damage" in ability_keys_data[i]:
-                if ability_keys_data[i][ability_keys_data[i].index("Damage") + 2] == "Physical" or \
-                        ability_keys_data[i][ability_keys_data[i].index("Damage") + 2] == "Magical" or \
-                        ability_keys_data[i][ability_keys_data[i].index("Damage") + 2] == "Pure":
-                    ability_keys_data[i].remove(ability_keys_data[i][ability_keys_data[i].index("Damage")+2])
-                    ability_keys_data[i].remove(ability_keys_data[i][ability_keys_data[i].index("Damage")+1])
-                elif ability_keys_data[i][ability_keys_data[i].index("Damage") + 1] == "Physical" or \
-                        ability_keys_data[i][ability_keys_data[i].index("Damage") + 1] == "Magical" or \
-                        ability_keys_data[i][ability_keys_data[i].index("Damage") + 1] == "Pure":
-                    ability_keys_data[i].remove(ability_keys_data[i][ability_keys_data[i].index("Damage")+1])
-                ability_keys_data[i].remove("Damage")
-            if "Modifiers" in ability_keys_data[i]:
-                ability_keys_data[i] = ability_keys_data[i][:ability_keys_data[i].index("Modifiers")]
-
-        # Index iterator through ability values list
-        value_index = 0
-
-        ability_dict = {}
-
-        # Creates each ability's table from cleaned data
-        for i in range(len(ability)):
-
-            ability_table = PrettyTable(['Ability Name', ability[i]])
-
-            for j in range(int(len(ability_header_data[i])/2)):
-                ability_table.add_row([ability_header_data[i][j*2].strip(), ability_header_data[i][j*2+1].strip()])
-
-            for j in range(len(ability_keys_data[i])):
-                if ability_keys_data[i][j] == "Cast Animation":
-                    ability_table.add_row([ability_keys_data[i][j].strip(), ability_values[value_index].strip() + '+' +
-                                           ability_values[value_index+1].strip()])
-                    value_index += 2
-                else:
-                    ability_table.add_row([ability_keys_data[i][j].strip(), ability_values[value_index].strip()])
-                    value_index += 1
-
-            if cd_mana_data[i]:
-                ability_table.add_row(['Cooldown', cd_mana_data[i][0]])
-
-            if len(cd_mana_data[i]) == 2:
-                ability_table.add_row(['Mana Cost', cd_mana_data[i][1]])
-
-            # Aligns ability table to left
-            ability_table.align = "l"
-
-            ability_dict[ability[i]] = ability_table
-
-            print(ability_dict[ability[i]])
-
-            ability_table.clear_rows()
-
-        yield ability_dict
 
     def parse_title(self, response):
 
@@ -259,7 +70,7 @@ class Dota2wikiSpider(scrapy.Spider):
                                '[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//th'
                                '/text()').extract()[1].strip()
 
-        yield title
+        return title
 
     def parse_lore(self, response):
 
@@ -267,7 +78,7 @@ class Dota2wikiSpider(scrapy.Spider):
         lore = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "biobox", " " ))]//'
                               'tr[(((count(preceding-sibling::*) + 1) = 4) and parent::*)]//td/text()').extract()
 
-        yield lore
+        return lore
 
     def parse_stat_gain(self, response):
 
@@ -283,7 +94,7 @@ class Dota2wikiSpider(scrapy.Spider):
                             stat[2] + stat[3],
                             stat[4] + stat[5]])
 
-        yield stat_table
+        return stat_table
 
     def parse_data(self, response):
         header = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "evenrowsgray", " " ))]'
@@ -365,7 +176,7 @@ class Dota2wikiSpider(scrapy.Spider):
             misc_table.add_row([misc_key[index], misc_val[index]])
             index += 1
 
-        yield misc_table
+        return misc_table
 
     def parse_abilities(self, response):
         # Normal abilities
@@ -605,7 +416,7 @@ class Dota2wikiSpider(scrapy.Spider):
             # Clears table rows for next table
             ability_table.clear_rows()
 
-        yield ability_dict
+        return ability_dict
 
     def parse_talent_tree(self, response):
 
@@ -641,4 +452,4 @@ class Dota2wikiSpider(scrapy.Spider):
             level -= 5
             index += 2
 
-        yield talent_table
+        return talent_table
